4f18ad338dd858c29ca61e1fc5fa18a000d48d9f
ayesha <ayesha.azmi@brainstation-23.com>
2024-10-06 19:22:00 +0600

    Created discovery service

---
+++ discovery/src/test/java/com/project/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
